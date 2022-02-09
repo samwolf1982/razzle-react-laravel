568362b261d74b5016cfd220c3dd5bdf3937ea1f
sam <biryukwolf1@gmail.com>
2022-02-09 14:48:14 +0200

    --wip-- [skip ci]

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ https://docs.docker.com/compose/env-file/
 hosts
 127.0.0.1	front.local.com
 127.0.0.1	api.local.com
+
+
+cp app-front/example.env.local app-front/.env.local
+
+cp app-back/.env.example app-back/.env
+
+todo need some better
+sudo chmod -R 777 ./app-back/storage
+sudo chmod -R 777 ./app-back/bootstrap/cache/
--- app-front/src/server.js
+++ app-front/src/server.js
@@ -29,7 +29,7 @@ export const renderApp = (req, res) => {
   );
   // const markup = '';
   const html = `<!doctype html><html lang=""><head><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta charset="utf-8" /><title>Razzle react laravel</title><meta name="viewport" content="width=device-width, initial-scale=1">${cssLinksFromAssets(assets, 'client')}</head><body><div id="root">${markup}</div>${jsScriptTagsFromAssets(assets, 'client', 'defer', 'crossorigin')}</body></html>`
-    console.log("html",html)
+
   return {context, html};
 }
 
